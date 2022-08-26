d6fc862a6fb220a146c765b705ce5df3d01ecd45
Roman <70903505+RomanZakharchuk@users.noreply.github.com>
2022-08-26 16:33:24 +0300

    made main-video component

--- src/components/MainVideo/MainVideo.jsx
+++ src/components/MainVideo/MainVideo.jsx
@@ -1,7 +1,24 @@
 
+const src = "https://www.youtube.com/embed/xVK7t7kFg4E";
+
 const MainVideo = () => {
     return (
-        <div>MainVideo</div>
+        <section className="px-5">
+            <div className="w-full max-w-[1280px] mx-auto py-[40px]">
+                <h2 className="my-text-grey text-[1.875rem] md:[2.188rem] leading-[2.188rem] text-center mb-[45px]">Lorem ipsum dolor sit amet</h2>
+
+                <div>
+                    <iframe
+                        className="w-full max-w-[394px] md:max-w-[920px] h-[248px] md:h-[517px] mx-auto"
+                        src={src}
+                        title="YouTube video player"
+                        frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen
+                    ></iframe>
+                </div>
+            </div>
+        </section>
     )
 }
 
