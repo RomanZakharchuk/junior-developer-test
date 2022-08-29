46adfd0ca9e9e0421df70f8def843229a91048df
Roman <70903505+RomanZakharchuk@users.noreply.github.com>
2022-08-29 23:29:55 +0300

    made footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,15 @@
+import logo from '../../assets/svg/logo-small.svg';
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer className="bg-[#446A82] px-5">
+            <div className="w-full max-w-[1280px] mx-auto py-12 flex items-center justify-center">
+                <div className='md:mr-4'>
+                    <img className='hidden md:block' src={logo} alt="logo" />
+                </div>
+                <p className='my-text-white text-base leading-6'>© 2020 Salmiac | All Rights Reserved</p>
+            </div>
+        </footer>
     )
 }
 
